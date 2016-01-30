8874895b0226da7e103cc082c72ccdeaf2f8a9e3
alejandro gallo <alejandroamsg@hotmail.com>
2016-01-30 18:49:52 +0100

    updated listing

--- bin/list-plugins.sh
+++ bin/list-plugins.sh
@@ -2,4 +2,5 @@
 
 SCRIPT_DIR=$(dirname $0) 
 
-grep "Plugin " "$SCRIPT_DIR/../files/.vimrc"  | sed -e "s+[a-z]*[0-9]*/++" -e "s/Plugin//" | tr -d "'" | tr -d " "
+echo -e "====> \033[4;31mList plugins in the vimrc file\033[0m"
+grep "Plugin " "$SCRIPT_DIR/../etc/vimrc"  | sed -e "s+[a-z]*[0-9]*/++" -e "s/Plugin//" | tr -d "'" | tr -d " "
